1f8ed529d8cbf77bf45b8d6d8392c19c677eef8f
AdeTiDotun <adejumodaniel274@gmail.com>
2024-03-04 23:09:24 +0100

    counter 2

---
+++ counter2.js
@@ -0,0 +1,13 @@
+function createCounter (init) {
+    let counter = init;
+
+   function increment() {
+       return counter++;
+    }
+
+   function decrement () {
+        return counter--;
+    }
+
+    
+}
